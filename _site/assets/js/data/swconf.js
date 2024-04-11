4ea6ab66573348773d44f212ca4d82f1e44f1ee4
alice102938 <155739672+alice102938@users.noreply.github.com>
2024-04-11 22:32:54 +0900

    test 사라짐

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1712841735',resources: [
+    cacheName: 'chirpy-1712842348',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
