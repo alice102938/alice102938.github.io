95642fd847b1f121b15d0bb14bbb1885a4c22573
alice102938 <155739672+alice102938@users.noreply.github.com>
2024-04-11 19:18:28 +0900

    error 16 수정

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1712830100',resources: [
+    cacheName: 'chirpy-1712830608',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
