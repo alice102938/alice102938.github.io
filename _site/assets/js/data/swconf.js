699adcea4b9ba10b2644c3fa575a42bde9ba3893
alice102938 <155739672+alice102938@users.noreply.github.com>
2024-05-03 22:53:21 +0900

    fix: post pin 수정

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1714235632',resources: [
+    cacheName: 'chirpy-1714744353',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
