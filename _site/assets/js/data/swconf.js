94d72ae55dcde4c9ccc6bf91585d326e289a5f1a
alice102938 <155739672+alice102938@users.noreply.github.com>
2024-05-04 16:21:07 +0900

    post: API 개발

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1714744673',resources: [
+    cacheName: 'chirpy-1714807227',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
